08437964e87d4e557aa13ae73d6f91992db54f59
Wang, Jijun <jijun.wang@intel.com>
2025-01-23 12:52:26 -0500

    11.7.0.2 Update 3
    - Updates spec to limit tcl_dev package version to less than 9
    - updated build script to get installation directories from rpm macros

--- OpenIb_Host/ff_install.sh
+++ OpenIb_Host/ff_install.sh
@@ -18,38 +18,45 @@ fi
 source OpenIb_Host/ff_filegroups.sh
 
 release_string="IntelEth-Tools-FF.$BUILD_TARGET_OS_ID.$MODULEVERSION"
+sbindir="$(rpm --eval "%{_sbindir}" | sed -e 's/^\///')"
+bindir="$(rpm --eval "%{_bindir}" | sed -e 's/^\///')"
+prefix="$(rpm --eval "%{_prefix}" | sed -e 's/^\///')"
+mandir="$(rpm --eval "%{_mandir}" | sed -e 's/^\///')"
+datadir="$(rpm --eval "%{_datadir}" | sed -e 's/^\///')"
+usrsrc="$(rpm --eval "%{_usrsrc}" | sed -e 's/^\///')"
+sysconfdir="$(rpm --eval "%{_sysconfdir}" | sed -e 's/^\///')"
 
-mkdir -p ${DESTDIR}/usr/bin
-mkdir -p ${DESTDIR}/usr/sbin
-mkdir -p ${DESTDIR}/usr/lib/eth-tools
-mkdir -p ${DESTDIR}/usr/share/eth-tools/{samples,help}
+mkdir -p ${DESTDIR}/${bindir}
+mkdir -p ${DESTDIR}/${sbindir}
+mkdir -p ${DESTDIR}/${prefix}/lib/eth-tools
+mkdir -p ${DESTDIR}/${datadir}/eth-tools/{samples,help}
 #mkdir -p ${DESTDIR}/${DSAP_LIBDIR}/ibacm
-mkdir -p ${DESTDIR}/etc/rdma
-mkdir -p ${DESTDIR}/etc/eth-tools
+mkdir -p ${DESTDIR}/${sysconfdir}/rdma
+mkdir -p ${DESTDIR}/${sysconfdir}/eth-tools
 #mkdir -p ${DESTDIR}/etc/cron.d
 #mkdir -p ${DESTDIR}/usr/include/infiniband
-mkdir -p ${DESTDIR}/usr/share/man/man1
-mkdir -p ${DESTDIR}/usr/share/man/man8
-mkdir -p ${DESTDIR}/usr/src/eth/mpi_apps
+mkdir -p ${DESTDIR}/${mandir}/man1
+mkdir -p ${DESTDIR}/${mandir}/man8
+mkdir -p ${DESTDIR}/${usrsrc}/eth/mpi_apps
 
 #Binaries and scripts installing (basic tools)
 #cd builtbin.OPENIB_FF.release
 cd $(cat ${BUILDDIR}/RELEASE_PATH)
 
 cd bin
-[ -n "$basic_tools_sbin" ] && cp -t ${DESTDIR}/usr/sbin $basic_tools_sbin
-[ -n "$basic_tools_opt" ] && cp -t ${DESTDIR}/usr/lib/eth-tools/ $basic_tools_opt
+[ -n "$basic_tools_sbin" ] && cp -t ${DESTDIR}/${sbindir} $basic_tools_sbin
+[ -n "$basic_tools_opt" ] && cp -t ${DESTDIR}/${prefix}/lib/eth-tools/ $basic_tools_opt
 #ln -s ./opaportinfo ${DESTDIR}/usr/sbin/opaportconfig
 
 cd ../bin
-[ -n "$ff_tools_opt" ] && cp -t ${DESTDIR}/usr/lib/eth-tools/ $ff_tools_opt
-[ -n "$opasnapconfig_bin" ] && cp -t ${DESTDIR}/usr/lib/eth-tools/ $opasnapconfig_bin
+[ -n "$ff_tools_opt" ] && cp -t ${DESTDIR}/${prefix}/lib/eth-tools/ $ff_tools_opt
+[ -n "$opasnapconfig_bin" ] && cp -t ${DESTDIR}/${prefix}}/lib/eth-tools/ $opasnapconfig_bin
 
 cd ../fastfabric
-[ -n "$ff_tools_sbin" ] && cp -t ${DESTDIR}/usr/sbin $ff_tools_sbin
-[ -n "$ff_tools_misc" ] && cp -t ${DESTDIR}/usr/lib/eth-tools/ $ff_tools_misc
-[ -n "$help_doc" ] && cp -t ${DESTDIR}/usr/share/eth-tools/help $help_doc
-[ -n "$basic_configs" ] && cp -t ${DESTDIR}/etc/eth-tools $basic_configs
+[ -n "$ff_tools_sbin" ] && cp -t ${DESTDIR}/${sbindir} $ff_tools_sbin
+[ -n "$ff_tools_misc" ] && cp -t ${DESTDIR}/${prefix}/lib/eth-tools/ $ff_tools_misc
+[ -n "$help_doc" ] && cp -t ${DESTDIR}/${datadir}/eth-tools/help $help_doc
+[ -n "$basic_configs" ] && cp -t ${DESTDIR}/${sysconfdir}/eth-tools $basic_configs
 
 cd ../etc
 #cd cron.d
@@ -57,30 +64,30 @@ cd ../etc
 #cd ..
 
 cd ../fastfabric/samples
-([ -n "$ff_iba_samples" ] || [ -n "$basic_samples" ]) && cp -t ${DESTDIR}/usr/share/eth-tools/samples $ff_iba_samples $basic_samples
+([ -n "$ff_iba_samples" ] || [ -n "$basic_samples" ]) && cp -t ${DESTDIR}/${datadir}/eth-tools/samples $ff_iba_samples $basic_samples
 cd ..
 
 cd ../fastfabric/tools
-[ -n "$ff_tools_exp" ] && cp -t ${DESTDIR}/usr/lib/eth-tools/ $ff_tools_exp
-[ -n "$ff_libs_misc" ] && cp -t ${DESTDIR}/usr/lib/eth-tools/ $ff_libs_misc
-cp -t ${DESTDIR}/usr/lib/eth-tools/ osid_wrapper
-cp -t ${DESTDIR}/etc/eth-tools allhosts hosts switches
+[ -n "$ff_tools_exp" ] && cp -t ${DESTDIR}/${prefix}/lib/eth-tools/ $ff_tools_exp
+[ -n "$ff_libs_misc" ] && cp -t ${DESTDIR}/${prefix}/lib/eth-tools/ $ff_libs_misc
+cp -t ${DESTDIR}/${prefix}/lib/eth-tools/ osid_wrapper
+cp -t ${DESTDIR}/${sysconfdir}/eth-tools allhosts hosts switches
 cd ..
 
 cd ../man/man1
-[ -n "$basic_mans" ] && cp -t ${DESTDIR}/usr/share/man/man1 $basic_mans
-[ -n "$opasadb_mans" ] && cp -t ${DESTDIR}/usr/share/man/man1 $opasadb_mans
+[ -n "$basic_mans" ] && cp -t ${DESTDIR}/${mandir}/man1 $basic_mans
+[ -n "$opasadb_mans" ] && cp -t ${DESTDIR}/${mandir}/man1 $opasadb_mans
 cd ../man8
-[ -n "$ff_mans" ] && cp -t ${DESTDIR}/usr/share/man/man8 $ff_mans
+[ -n "$ff_mans" ] && cp -t ${DESTDIR}/${mandir}/man8 $ff_mans
 cd ..
 
 cd ../src/mpi/mpi_apps
-tar -xzf mpi_apps.tgz -C ${DESTDIR}/usr/src/eth/mpi_apps/
+tar -xzf mpi_apps.tgz -C ${DESTDIR}/${usrsrc}/eth/mpi_apps/
 cd ../../
 
 #Config files
 cd ../config
-cp -t ${DESTDIR}/etc/eth-tools ethmon.conf ethmon.si.conf
+cp -t ${DESTDIR}/${sysconfdir}/eth-tools ethmon.conf ethmon.si.conf
 
 #Libraries installing
 #cd ../builtlibs.OPENIB_FF.release
@@ -90,5 +97,5 @@ cd $(cat $BUILDDIR/LIB_PATH)
 # Now that we've put everything in the buildroot, copy any default config files to their expected location for user
 # to edit. To prevent nuking existing user configs, the files section of this spec file will reference these as noreplace
 # config files.
-cp ${DESTDIR}/usr/lib/eth-tools/ethfastfabric.conf.def ${DESTDIR}/etc/eth-tools/ethfastfabric.conf
+cp ${DESTDIR}/usr/lib/eth-tools/ethfastfabric.conf.def ${DESTDIR}/${sysconfdir}/eth-tools/ethfastfabric.conf
 
